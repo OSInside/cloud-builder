681faabe565457b776c447954128bb5e4a283ff1
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-06-23 20:23:33 +0200

    Initial commit

---
+++ cloud_builder/exceptions.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2021 Marcus Schäfer.  All rights reserved.
+# 
+# This file is part of Cloud Builder.
+#
+# Cloud Builder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cloud Builder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cloud Builder.  If not, see <http://www.gnu.org/licenses/>
+#
+import sys
+from functools import wraps
+from typing import Callable
+from cloud_builder.logger import CBLogger
+
+log = CBLogger.get_logger()
+
+
+def exception_handler(func: Callable) -> Callable:
+    """
+    Decorator method to add exception handling
+    Methods marked with this decorator are called under
+    control of the cloud builder exceptions
+
+    :param Callable func: Function pointer
+
+    :return: func, wrapped with exception handling
+
+    :rtype: Callable
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except CBError as issue:
+            # known exception, log information and exit
+            log.error(f'{type(issue).__name__}: {issue}')
+            sys.exit(1)
+        except KeyboardInterrupt:
+            log.error('Exit on keyboard interrupt')
+            sys.exit(1)
+        except SystemExit as issue:
+            # user exception, program aborted by user
+            sys.exit(issue)
+        except Exception:
+            # exception we did no expect, show python backtrace
+            log.error('Unexpected error:')
+            raise
+    return wrapper
+
+
+class CBError(Exception):
+    """
+    Base class to handle all known exceptions
+
+    Specific exceptions are implemented as sub classes of CBError
+    """
+    def __init__(self, message) -> None:
+        """
+        Store exception message
+
+        :param str message: Exception message text
+        """
+        self.message = message
+
+    def __str__(self) -> str:
+        """
+        Return representation of exception message
+
+        :return: A message
+
+        :rtype: str
+        """
+        return format(self.message)
