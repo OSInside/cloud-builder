acaaa5907a603263e44157cd0c391d3bab6e2bd6
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-06-25 22:53:30 +0200

    no sleep break
    kafka scale out to consumers according to the kafka
    config. See: https://stackoverflow.com/questions/40326600/balancing-kafka-consumers/40327547

--- cloud_builder/kafka.py
+++ cloud_builder/kafka.py
@@ -81,7 +81,7 @@ class CBKafka:
         # We want this message to go out now
         message_broker.flush()
 
-    def read_request(self, timeout_ms=10) -> kafka_read_type:
+    def read_request(self, timeout_ms=1000) -> kafka_read_type:
         """
         Read messages from kafka. The message has to be valid
         YAML and has to follow the request_schema in order to
@@ -137,7 +137,7 @@ class CBKafka:
         """
         consumer.close()
 
-    def read(self, timeout_ms=10) -> kafka_read_type:
+    def read(self, timeout_ms=1000) -> kafka_read_type:
         """
         Read messages from kafka.
 
