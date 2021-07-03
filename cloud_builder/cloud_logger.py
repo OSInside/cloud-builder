6be82a92d70e9f491bb4a7a0630aa056650e63c5
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-07-03 19:09:03 +0200

    Tests for cloud_logger

--- cloud_builder/cloud_logger.py
+++ cloud_builder/cloud_logger.py
@@ -24,24 +24,60 @@ from typing import Dict
 
 class CBCloudLogger:
     def __init__(self, service: str, name: str) -> None:
+        """
+        Create a logger object for local logging and
+        logging of response messages to the message broker
+        response queue
+
+        :param str service: service name
+        :param str name: custom name
+        """
         self.log = CBLogger.get_logger(
             logfile=Defaults.get_cb_logfile()
         )
         self.id = CBIdentity.get_id(service, name)
 
     def get_id(self) -> str:
+        """
+        Return log prefix ID
+
+        :return: ID string from CBIdentity.get_id
+
+        :rtype: str
+        """
         return self.id
 
     def info(self, message: str) -> None:
+        """
+        Local log for info message
+
+        :param str message: message
+        """
         self.log.info(f'{self.id}: {message}')
 
     def warning(self, message: str) -> None:
-        self.log.info(f'{self.id}: {message}')
+        """
+        Local log for warning message
+
+        :param str message: message
+        """
+        self.log.warning(f'{self.id}: {message}')
 
     def error(self, message: str) -> None:
+        """
+        Local log for error message
+
+        :param str message: message
+        """
         self.log.error(f'{self.id}: {message}')
 
     def response(self, message: Dict) -> None:
+        """
+        Message broker plus local info log for
+        response message
+
+        :param dict message: yaml dict message
+        """
         self.log.info(
             '{0}: {1}'.format(self.id, yaml.dump(message).encode())
         )
