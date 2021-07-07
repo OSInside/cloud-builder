9b47f4c9914521ed6e264042a6503fb4cfce604d
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-07-07 17:36:00 +0200

    Add tests for response messages

---
+++ test/unit/response_test.py
@@ -0,0 +1,105 @@
+from cloud_builder.response import CBResponse
+
+
+class TestCBResponse:
+    def setup(self):
+        self.response = CBResponse('uuid', 'identity')
+
+    def test_set_package_build_response(self):
+        self.response.set_package_build_response(
+            'message', 'response_code', 'package', 'log_file', [], 0
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'log_file': 'log_file',
+            'binary_packages': [],
+            'exit_code': 0
+        }
+
+    def test_set_package_buildroot_response(self):
+        self.response.set_package_buildroot_response(
+            'message', 'response_code', 'package', 'log_file', 'build_root', 0
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'log_file': 'log_file',
+            'build_root': 'build_root',
+            'exit_code': 0
+        }
+
+    def test_set_package_update_request_response(self):
+        self.response.set_package_update_request_response(
+            'message', 'response_code', 'package', 'arch'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'arch': 'arch'
+        }
+
+    def test_set_package_build_scheduled_response(self):
+        self.response.set_package_build_scheduled_response(
+            'message', 'response_code', 'package', 'arch'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'arch': 'arch'
+        }
+
+    def test_set_buildhost_arch_incompatible_response(self):
+        self.response.set_buildhost_arch_incompatible_response(
+            'message', 'response_code', 'package', 'arch'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'arch': 'arch'
+        }
+
+    def test_set_package_jobs_reset_response(self):
+        self.response.set_package_jobs_reset_response(
+            'message', 'response_code', 'package', 'arch'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'arch': 'arch'
+        }
+
+    def test_set_package_not_existing_response(self):
+        self.response.set_package_not_existing_response(
+            'message', 'response_code', 'package', 'arch'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package',
+            'arch': 'arch'
+        }
+
+    def test_set_package_invalid_metadata_response(self):
+        self.response.set_package_invalid_metadata_response(
+            'message', 'response_code', 'package'
+        )
+        assert self.response.get_data() == {
+            **self.response.response_dict,
+            'message': 'message',
+            'response_code': 'response_code',
+            'package': 'package'
+        }
