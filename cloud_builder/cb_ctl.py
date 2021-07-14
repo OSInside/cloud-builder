34bddcc8e4359a072dc719fab33434d317bc868c
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-07-14 11:43:20 +0200

    Fill cb-ctl with life

--- cloud_builder/cb_ctl.py
+++ cloud_builder/cb_ctl.py
@@ -17,22 +17,50 @@
 #
 """
 usage: cb-ctl -h | --help
-       cb-ctl --build=<package>
+       cb-ctl --build=<package> --project-path=<path> --arch=<name> --dist=<name>
        cb-ctl --info=<package>
            [--timeout=<time_sec>]
        cb-ctl --watch --request-id=<uuid>
            [--timeout=<time_sec>]
 
 options:
+    --build=<package>
+        Create a request to build the given package.
+        The provided argument is appended to the
+        project-path and forms the directory path
+        to the package in the git repository
+
+        projects/
+        └── <project-path>/
+                    └── <package>/...
+
+        Please note, the root directory is by convention
+        a fixed name set to 'projects'
+
+    --project-path=<path>
+        Project path that points to the package in the git.
+        See the above structure example
+
+    --arch=<name>
+        Target architecture name
+
+    --dist=<name>
+        Target distribution name
+
     --timeout=<time_sec>
         Wait time_sec seconds of inactivity on the message
         broker before return. Default: 30sec
 """
+import os
 from docopt import docopt
+from typing import Any
+
 from cloud_builder.version import __version__
 from kiwi.privileges import Privileges
-# from cloud_builder.defaults import Defaults
-# from cloud_builder.package_request.package_request import CBPackageRequest
+from cloud_builder.defaults import Defaults
+from cloud_builder.broker import CBMessageBroker
+from cloud_builder.package_request.package_request import CBPackageRequest
+from cloud_builder.utils.display import CBDisplay
 from cloud_builder.exceptions import exception_handler
 
 
@@ -49,25 +77,64 @@ def main() -> None:
 
     Privileges.check_for_root_permissions()
 
+    broker = CBMessageBroker.new(
+        'kafka', config_file=Defaults.get_kafka_config()
+    )
+
     if args['--build']:
-        build_package(args['--build'])
+        build_package(
+            broker,
+            args['--build'],
+            args['--project-path'],
+            args['--arch'],
+            args['--dist']
+        )
     elif args['--info']:
-        get_package_info(args['--info'], args['--timeout'])
+        get_package_info(
+            broker,
+            args['--info'],
+            args['--timeout']
+        )
     elif args['--watch']:
-        get_response_for_request(args['--request-id'], args['--timeout'])
+        get_response_for_request(
+            broker,
+            args['--request-id'],
+            args['--timeout']
+        )
 
 
-def build_package(packge: str) -> None:
-    # broker = CBMessageBroker.new(
-    #     'kafka', config_file=Defaults.get_kafka_config()
-    # )
-    # package_request = CBPackageRequest()
+def build_package(
+    broker: Any, package: str, project_path: str, arch: str, dist: str
+) -> None:
+    package_request = CBPackageRequest()
+    package_request.set_package_build_request(
+        os.path.join(
+            'projects', project_path, package,
+        ), arch, dist,
+        Defaults.get_status_flags().package_update_request
+    )
+    broker.send_package_request(package_request)
+    CBDisplay.print_yaml(package_request.get_data())
+    broker.close()
+
+
+def get_package_info(
+    broker: Any, package: str, timeout_sec: int
+) -> None:
+    # TODO
     pass
 
 
-def get_package_info(package: str, timeout_sec: int) -> None:
-    pass
-
-
-def get_response_for_request(request_id: str, timeout_sec: int) -> None:
-    pass
+def get_response_for_request(
+    broker: Any, request_id: str, timeout_sec: int
+) -> None:
+    for message in broker.read(
+        topic=Defaults.get_response_queue_name(),
+        group=f'cb-ctl:watch:{os.getpid()}',
+        timeout_ms=int(timeout_sec * 1000)
+    ):
+        response = broker.validate_response_request(message.value)
+        if response:
+            broker.acknowledge()
+            if response['request_id'] == request_id:
+                CBDisplay.print_yaml(response)
