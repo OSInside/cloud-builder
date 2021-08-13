e40ab75fc0b736187b58155e8d5e797c88249cea
ms <ms@bots.github.com>
2021-08-13 19:24:19 +0200

    Bump version: 0.1.21 → 0.1.22

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,7 +108,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.21'
+version = '0.1.22'
 # The full version, including alpha/beta/rc tags.
 release = version
 
