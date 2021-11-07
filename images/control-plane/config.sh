0e989a3530a68833662ef90990348b30f22883cc
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-11-07 16:19:34 +0100

    Update control-plane image description
    Several fixes, services, cloud-init

--- images/control-plane/config.sh
+++ images/control-plane/config.sh
@@ -1,16 +1,9 @@
+#!/bin/bash
+set -e
+
 test -f /.kconfig && . /.kconfig
 
 echo "Configure image: [$kiwi_iname]..."
-
-# sysconfig settings
-echo '# Support dynamic multinic configuration' \
-    >> /etc/sysconfig/network/config
-net_modules="cloud-netconfig dns-resolver dns-bind dns-dnsmasq nis ntp-runtime"
-echo "NETCONFIG_MODULES_ORDER=\"$net_modules\"" \
-    >> /etc/sysconfig/network/config
-
-# Setup policy kit
-[ -x /sbin/set_polkit_default_privs ] && /sbin/set_polkit_default_privs
 
 # Disable password based login via ssh
 ssh_option=ChallengeResponseAuthentication
@@ -18,21 +11,23 @@ sed -i "s/#${ssh_option} yes/${ssh_option} no/" \
     /etc/ssh/sshd_config
 
 # Activate services
-suseInsertService boot.device-mapper
-suseInsertService haveged
-suseInsertService sshd
-suseInsertService cloud-netconfig.timer
-
-# Activate services CB
-suseInsertService cb-fetch
-
-# Activate/De-activeta services
+baseInsertService dbus-broker
+baseInsertService NetworkManager
+baseInsertService sshd
 baseInsertService chronyd
 baseInsertService cloud-init-local
 baseInsertService cloud-init
 baseInsertService cloud-config
 baseInsertService cloud-final
 
+# Activate services CB
+suseInsertService cb-fetch
+
 # Fix permissions
 chown -R fedora /home/fedora
 chgrp -R fedora /home/fedora
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
