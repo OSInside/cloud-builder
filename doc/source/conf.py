691bfa03bc0c8ee73f310faae999eb5568743d6c
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-11-23 15:04:36 +0100

    Bump version: 0.2.48 → 0.2.49

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,7 +108,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.48'
+version = '0.2.49'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from os import path
 from setuptools import setup
   
-__version__ = '0.2.48'
+__version__ = '0.2.49'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as readme:
