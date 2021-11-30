515a8cfbfcdd79d8cb1a80a19b96883cf73268bf
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-11-30 22:00:53 +0100

    Bump version: 0.2.57 → 0.2.58

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from os import path
 from setuptools import setup
   
-__version__ = '0.2.57'
+__version__ = '0.2.58'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as readme:
